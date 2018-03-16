05d942aa06ac2686281d32f24da508c83f659af0
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-03-16 17:20:22 +0100

    Added option to give debug-bridge commands

--- python/plp_board_runner.py
+++ python/plp_board_runner.py
@@ -84,10 +84,14 @@ class Runner(Platform):
 
 
         if self.system_tree.get('pulp_chip') in ['fulmine', 'gap', 'wolfe']:
+
+            commands = " ".join(self.system_tree.get('debug-bridge/commands').split(','))
+
+
             if self.system_tree.get('pulp_chip') in ['gap']:
-                return execCmd('plpbridge --verbose --cable=ftdi@digilent --boot-mode=jtag --binary=%s --chip=gap load ioloop start wait' % (binary))
+                return execCmd('plpbridge --verbose --cable=ftdi@digilent --boot-mode=jtag --binary=%s --chip=gap %s' % (binary, commands))
             else:
-                return execCmd('plpbridge --verbose --binary=%s --config=%s load ioloop start wait' % (binary, self.config.getOption('configFile')))
+                return execCmd('plpbridge --verbose --binary=%s --config=%s %s' % (binary, self.config.getOption('configFile'), commands))
 
         elif self.config.getOption('pulpArchi') == 'pulp3':
             return execCmd("debug_bridge -c ft2232 -b %s --binary %s --load --late-reset --loop --printf --start %s %s" % (self.config.getOption('pulpArchi').replace('-riscv', ''), binary, mask, flashOpt))
