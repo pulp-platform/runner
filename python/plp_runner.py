86b12be4b045bcc06de1c49a92ddeb061ed10107
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-08-13 09:03:34 +0200

    Switched all chips to new vp

--- python/plp_runner.py
+++ python/plp_runner.py
@@ -212,7 +212,7 @@ class Runner(object):
 
         platform_name = config.get('**/platform').get()
 
-        if platform_name == 'gvsoc' and self.system_tree.get('pulp_chip') in ['pulp', 'pulpissimo', 'oprecompkw', 'oprecompkw_sfloat', 'oprecompkw_sfloat_sa', 'multino', 'oprecompkw_sa', 'bigpulp', 'bigpulp-z-7045', 'wolfe']:
+        if platform_name == 'gvsoc' and self.system_tree.get('pulp_chip') not in []:
             platform_name = 'vp'
 
         try:
