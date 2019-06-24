067e1a11f102dd66a00bb09fac9f1afff151f2aa
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-06-24 17:12:38 +0200

    Now using either PULP_RISCV_GCC_TOOLCHAIN or PULP_RISCV_GCC_TOOLCHAIN_CI when using pulpissimo fpga

--- python/runner/fpga/pulpissimo/elf_run_genesys2.sh
+++ python/runner/fpga/pulpissimo/elf_run_genesys2.sh
@@ -3,6 +3,10 @@
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
+toolchain=$PULP_RISCV_GCC_TOOLCHAIN_CI
+if [ -z "$toolchain" ]; then
+	toolchain=$PULP_RISCV_GCC_TOOLCHAIN
+fi
 
 SCRIPTDIR=$(dirname $0)
 UART_TTY=${PULP_GENESYS2_UART_TTY:=/dev/ttyUSB0}
@@ -10,7 +14,7 @@ UART_BAUDRATE=${PULP_GENESYS2_UART_BAUDRATE:=115200}
 
 #Execute gdb and connect to openocd via pipe
 $OPENOCD/bin/openocd -f $SCRIPTDIR/openocd-genesys2.cfg &
-$PULP_RISCV_GCC_TOOLCHAIN_CI/bin/riscv32-unknown-elf-gdb -x $SCRIPTDIR/elf_run.gdb $1 &
+$toolchain/bin/riscv32-unknown-elf-gdb -x $SCRIPTDIR/elf_run.gdb $1 &
 sleep 3
 minicom -D $UART_TTY -b $UART_BAUDRATE
 
